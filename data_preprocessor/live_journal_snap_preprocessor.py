344e6393014a7d8d832790fd5ea32ed48a52de50
GuanSuns <guansuns@outlook.com>
2019-10-30 23:58:03 -0700

    implemented the node2vec random walk sampling

--- data_preprocessor/live_journal_snap_preprocessor.py
+++ data_preprocessor/live_journal_snap_preprocessor.py
@@ -1,14 +1,14 @@
-import data_preprocessor.snap_community_preprocessor as snap_preprocessor
+from data_preprocessor.snap_community_preprocessor import load_graph_from_txt, save_graph_to_edge_list
 
 
 def main():
     graph_file = '../raw_data/live-journal-snap/com-lj.ungraph.txt'
     community_file = '../raw_data/live-journal-snap/com-lj.all.cmty.txt'
     top5000_community_file = '../raw_data/live-journal-snap/com-lj.top5000.cmty.txt'
-    graph, node_info = snap_preprocessor.load_graph_from_txt(graph_file, community_file, top5000_community_file)
+    graph, node_info = load_graph_from_txt(graph_file, community_file, top5000_community_file)
     # save the graph into edgelist
     # save the top community_info to txt, format: node_id [list of community it belongs to]
-    snap_preprocessor.save_graph_to_edge_list(graph, node_info, fname='live-journal-snap')
+    save_graph_to_edge_list(graph, node_info, fname='live-journal-snap')
 
 
 if __name__ == '__main__':
