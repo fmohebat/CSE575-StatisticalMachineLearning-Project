a08fe875cab31b370bc599d4dba2dc0267c23283
Nathan Beste <ndbeste@gmail.com>
2019-11-16 17:15:40 -0700

    Automate Classification Experiment

--- run_classification_experiment.py
+++ run_classification_experiment.py
@@ -1,7 +1,7 @@
-import sys
+import sys, csv
 import argparse
 from evaluation import evaluation_utils
-import pandas as pd
+# import pandas as pd
 
 from gem.evaluation.evaluate_node_classification import TopKRanker
 
@@ -15,9 +15,9 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
 
-def run_classification_experiment(emb_file,label_file):
+def run_classification_experiment(emb_file,label_file, t_size=0.2):
     seed = 0
-    print(emb_file, label_file)
+    print(emb_file.split('/')[-1], label_file.split('/')[-1])
 
     # Pre-processing of emb_file and label_file
     patterns_sorted = evaluation_utils.emb_file_to_df(emb_file)
@@ -25,7 +25,7 @@ def run_classification_experiment(emb_file,label_file):
 
     # Creating train test split 80% 20%
     X_train, X_test, y_train, y_test = train_test_split(patterns_sorted.values.T, encoded_sorted.values.T, 
-                                                    test_size=.20,random_state=seed)
+                                                    test_size=t_size,random_state=seed)
     
     clf_list = [
         DecisionTreeClassifier(random_state=seed),
@@ -37,10 +37,13 @@ def run_classification_experiment(emb_file,label_file):
         TopKRanker(LogisticRegression(solver='lbfgs')) # OneVsRest
         ] 
     
+    l = []
     for clf in clf_list:
         print(clf.__class__.__name__, "\n")
-        evaluate_clf(clf, X_train, X_test, y_train, y_test)
+        (macro_f1, micro_f1) = evaluate_clf(clf, X_train, X_test, y_train, y_test)
+        l.append((emb_file.split('/')[-1],clf.__class__.__name__,t_size,macro_f1, micro_f1))
         print("\n\n")
+    return l
     
     
     # Writing micro/macro F1 results in a tabular form to a csv output
@@ -56,12 +59,31 @@ def evaluate_clf(clf, X_train, X_test, y_train, y_test):
     if prediction is None:
         print("run_classification_experiment Error - prediction was not run.")
         return
-    print("Macro f1: ", f1_score(y_test, prediction, average='macro'))
-    print("Micro f1: ", f1_score(y_test, prediction, average='micro'))
+    macro_f1 = f1_score(y_test, prediction, average='macro')
+    micro_f1 = f1_score(y_test, prediction, average='micro')
+    print("Macro f1: ", macro_f1)
+    print("Micro f1: ", micro_f1)
+    return (macro_f1, micro_f1)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("emb_file", help="path to emb file to use for classification, ex. output/karate/karate_node2vec.emb")
-    parser.add_argument("label_file", help="path to labels corresponding to embedding file, ex. labels/blog3.labels")
-    
-    run_classification_experiment(**vars(parser.parse_args()))
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("emb_file", help="path to emb file to use for classification, ex. output/karate/karate_node2vec.emb")
+    # parser.add_argument("label_file", help="path to labels corresponding to embedding file, ex. labels/blog3.labels")
+
+    # run_classification_experiment(**vars(parser.parse_args()))
+
+    emb_files = [
+    "blog-catalog_Node2Vec-Embedding.emb",
+    "blog-catalog_FastText-Embedding.emb",
+    "blog-catalog_hope_gsvd.emb" ]
+
+    emb_file_dir = "/Users/nathan/Documents/ASU/CSE/575/Project/SMLProject/CSE575-StatisticalMachineLearning-Project/output/blog-catalog/"
+    label_file = "/Users/nathan/Documents/ASU/CSE/575/Project/SMLProject/CSE575-StatisticalMachineLearning-Project/labels/blog-catalog-labels.txt"
+
+    csv_file = csv.writer(open('emb_output.csv', 'w'))
+
+    for emb_file in emb_files:
+        for i in range(1,10):
+            l = run_classification_experiment(emb_file_dir+emb_file, label_file, i/10)
+            for row in l:
+                csv_file.writerow(row)
